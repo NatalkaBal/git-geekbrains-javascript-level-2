d5845688de7b26ab14dd5197ae20b1b6aca5abf5
NatalkaBal <N_Balashova@list.ru>
2020-06-28 17:34:30 +0300

    lesson-4
    1. Добавить методы и обработчики событий для поля поиска. Создать в объекте данных поле searchLine и привязать к нему содержимое поля ввода. На кнопку «Искать» добавить обработчик клика, вызывающий метод FilterGoods.
    2. Добавить корзину. В html-шаблон добавить разметку корзины. Добавить в объект данных поле isVisibleCart, управляющее видимостью корзины.
    3. *Добавлять в .goods-list заглушку с текстом «Нет данных» в случае, если список товаров пуст.

--- Project-1/js/script.js
+++ Project-1/js/script.js
@@ -1,151 +1,103 @@
 'use strict';
 
-const mainHeader = document.getElementById('main-header');
-const mainFooter = document.getElementById('main-footer');
-const goodsListSection = document.getElementById('goods-list');
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-class contactsItem {
-    constructor(link, img_class_fontawesome) {
-        this.link = link;
-        this.img_class_fontawesome = img_class_fontawesome;
-    }
-    render() {
-        return `<a href=${this.link}><i class="${this.img_class_fontawesome}"></i></a>`
-    }
-}
+let app = new Vue ({
+    el: '#app',
 
-class contactsList {
-    constructor(){
-        this.contactsArray = [];
-    }
-    fetchContacts() {
-        this.contactsArray = [
-            { link: '#', img_class_fontawesome: "fab fa-facebook-square"},
-            { link: '#', img_class_fontawesome: 'fab fa-twitter-square'},
-            { link: '#', img_class_fontawesome: 'fab fa-instagram-square'},
-            { link: '#', img_class_fontawesome: 'fab fa-vk'},
-        ];
-    }
-    render() {
-        let listHtml = '';
-        this.contactsArray.forEach(({ link, img_class_fontawesome }) => {
-            const contactItem = new contactsItem(link, img_class_fontawesome);
-            listHtml += contactItem.render();
-        });
-        for (let elem of document.querySelectorAll('.main-contact')) {
-            elem.innerHTML = listHtml;
+    data: {
+        goods: [],
+        filteredGoods: [],
+        basket: [],
+        searchLine: '',
+        searchText: '',
+        sumGood: ''
+    },
+
+    methods: {
+
+        makeGETRequest(url, callback) {
+            return new Promise((resolve, reject) => {
+                let xhr;
+                if (window.XMLHttpRequest) {
+                    xhr = new XMLHttpRequest();
+                } else if (window.ActiveXObject) {
+                    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        callback(xhr.responseText);
+                    }
+                }
+                xhr.open('GET', url, true);
+                xhr.send();
+         });
+        },
+
+        sumGoods(){
+            let sum = 0;
+            this.basket.forEach(({price}) => {
+                sum += price;
+            });
+            this.sumGood = sum;
+        },
+
+        addProduct (good) {
+            this.basket.push(good);
+            this.sumGoods();
+        },
+
+        deleteProduct (good){
+            this.basket = this.basket.filter(item => item !== good);
+            this.sumGoods();
+        },
+
+        filterGoods () {
+            let text = this.searchLine.toLowerCase().trim();
+            if (text === '') {
+                this.filteredGoods = this.goods;
+            } else {
+                this.filteredGoods = this.goods.filter((el) => {
+                    return el.product_name.toLowerCase().includes(text);
+                });
+            }
+        },
+
+        clearfilterGoods(){
+            this.filteredGoods = this.goods;
+            this.searchLine = '';
         }
-        
-        mainHeader.style.display = 'flex';
-        mainHeader.style.justifyContent = 'space-between';
-        mainHeader.style.alignItems = 'center';
+    },
 
-        mainFooter.style.display = 'flex';
-        mainFooter.style.justifyContent = 'space-between';
-        mainFooter.style.alignItems = 'center';
+    mounted () {
+        this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+            this.goods = JSON.parse(goods);
+            this.filteredGoods = JSON.parse(goods);
+        });
     }
-}
-
-const contactlist = new contactsList();
-contactlist.fetchContacts();
-contactlist.render();
+});
 
 /////////////////////////////////////
-class GoodsItem {
-    constructor(title, price, srcpath) {
-        this.title = title;
-        this.price = price;
-        this.srcpath = srcpath;
+//заполним и выведим контакты в header
+const contactsArray = [
+    { link: '#', img_class_fontawesome: "fab fa-facebook-square"},
+    { link: '#', img_class_fontawesome: 'fab fa-twitter-square'},
+    { link: '#', img_class_fontawesome: 'fab fa-instagram-square'},
+    { link: '#', img_class_fontawesome: 'fab fa-vk'},
+];
+
+const renderContacts = (link = '#', img_class_fontawesome = 'noclass') => {
+    return `<a href=${link}><i class="${img_class_fontawesome}"></i></a>`;
+};
+
+const addContact = (contacts) => {
+    const contactList = contacts.map(contactItem => renderContacts(contactItem.link, contactItem.img_class_fontawesome));
+
+    for (let elem of document.querySelectorAll('.main-contact')) {
+        elem.innerHTML = contactList.join('');
     }
-    render() {
-        return `
-      <div id = "goods-item" class="goods-item">
-        <h3>${this.title}</h3>
-        <img src=${this.srcpath} alt="img goods">
-        <span>Цена: ${this.price}</span>
-      </div>
-    `
-    }
+
 }
 
-class GoodsList {
-    constructor(){
-        this.goods = [];
-    }
-    fetchGoods() {
-        this.goods = [
-            { title: 'Основы ООП', price: 5000, srcpath: 'img/oop.jpg'},
-            { title: 'CSS', price: 8000, srcpath: 'img/css.jpg'},
-            { title: 'HTML-5', price: 10000, srcpath: 'img/HTML5.jpg'},
-            { title: 'JavaScript', price: 12000, srcpath: 'img/js.jpg'},
-            { title: 'C++', price: 11000, srcpath: 'img/cpp-basic.jpg'},
-        ];
-    }
-    render() {
-        let listHtml = '';
-        this.goods.forEach(({ title, price, srcpath }) => {
-            const goodItem = new GoodsItem(title, price, srcpath);
-            listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
-
-        goodsListSection.style.display = 'flex';
-        goodsListSection.style.flexWrap = 'wrap';
-    }
-    summaGoods(){
-        let sum = 0;
-        this.goods.forEach(({title, price, srcpath}) => {
-                sum += price;
-        });
-        console.log('Сумма элементов: ' + sum);
-    }
-}
-
-const list = new GoodsList();
-list.fetchGoods();
-list.render();
-list.summaGoods();
-
-class basketItem {
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-
-    addBasket(){
-
-    }
-
-    deleteBasket(){
-
-    }
-
-    render() {
-        return `
-        <div id = "basket-item" class="basket-item">
-            <h3>${this.title}</h3>
-            <span>Цена: ${this.price}</span>
-        </div>
-    `
-    }
-}
-
-class basketList {
-    constructor(){
-        this.basketArray = [];
-    }
-
-    render() {
-        let listHtml = '';
-        this.basketArray.forEach(({ title, price }) => {
-            const baskItem = new basketItem(title, price);
-            listHtml += baskItem.render();
-        });
-    }
-
-    sumItem(){
-        
-    }
-}
-
-const basketlist = new basketList();
+addContact(contactsArray);
+/////////////////////////////////////
